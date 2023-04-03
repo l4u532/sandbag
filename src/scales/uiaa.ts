4848bb6167058dadce65081bee36b2063eb9cf7f
Kl4us <k.langenheldt@gmail.com>
2023-04-03 16:14:53 +0200

    expand routes.csv/.json with UIAA mapping

--- src/scales/uiaa.ts
+++ src/scales/uiaa.ts
@@ -1,9 +1,11 @@
 import GradeScale, { findScoreRange, getAvgScore, GradeScales, Tuple } from '../GradeScale'
-import boulder from '../data/boulder.json'
-import { Boulder } from '.'
-import { boulderScoreToBand, GradeBandTypes } from '../GradeBands'
+import routes from '../data/routes.json'
+import { Route } from '.'
+import { GradeBandTypes, routeScoreToBand } from '../GradeBands'
 
-const uiaaGradeRegex = /^(\d{1,2}[\+\-]?)|\d{1,2}$/
+const uiaaGradeRegex = /^(\d{1,2}\+?\/?\d?-?)+$/
+const isUIAA = (grade: string): RegExpMatchArray | null => grade.match(uiaaGradeRegex)
+
 // UIAA grading system, predominant in Central Europe (esp. Germany, Austria, Switzerland)
 // Uses Arabic numerals with +/- signs, e.g. "7", "7-" (easier), or "7+" (harder)
 // (Sometimes roman numerals, e. g. "VII", are used - this is not supported)
@@ -11,100 +13,63 @@ const uiaaGradeRegex = /^(\d{1,2}[\+\-]?)|\d{1,2}$/
 const UIAAScale: GradeScale = {
   displayName: 'UIAA Scale',
   name: GradeScales.UIAA,
-  offset: 1000, // TODO: what is this?
-  allowableConversionType: [GradeScales.UIAA], // TODO: what is this?
+  offset: 2000,
+  allowableConversionType: [GradeScales.YDS],
   isType: (grade: string): boolean => {
-    const isUIAAGrade = grade.match(uiaaGradeRegex) !== null
-    // If there isn't a match sort it to the bottom
-    if (isUIAAGrade === null) {
+    if (isUIAA(grade) === null) {
       return false
     }
     return true
   },
-  // TODO: what is this? seems to return normalised score for a grade
   getScore: (grade: string): number | Tuple => {
     return getScore(grade)
   },
   getGrade: (score: number | Tuple): string => {
-    function validateScore (score: number): number {
+    const validateScore = (score: number): number => {
       const validScore = Number.isInteger(score) ? score : Math.ceil(score)
-      return Math.min(Math.max(0, validScore), boulder.length - 1)
+      return Math.min(Math.max(0, validScore), routes.length - 1)
     }
 
     if (typeof score === 'number') {
-      return boulder[validateScore(score)].v
+      return routes[validateScore(score)].uiaa
     }
 
-    const low: string = boulder[validateScore(score[0])].v
-    const high: string = boulder[validateScore(score[1])].v
+    const low: string = routes[validateScore(score[0])].uiaa
+    const high: string = routes[validateScore(score[1])].uiaa
     if (low === high) return low
-    return `${low}-${high}`
+    return `${low}/${high}`
   },
   getGradeBand: (grade: string): GradeBandTypes => {
     const score = getScore(grade)
-    return boulderScoreToBand(getAvgScore(score))
+    return routeScoreToBand(getAvgScore(score))
   }
-}
-
-export default UIAAScale
-
-export const getScoreIrregular = (irregularMatch: RegExpMatchArray): number => {
-  let score = 0
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  const [_, group] = irregularMatch
-  switch (group) {
-    case 'easy':
-      score = 1
-      break
-    default:
-      score = 0
-      break
-  }
-  return score
-}
-
-export const getScoreCommon = (match: RegExpMatchArray): number => {
-  let score = 0
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  const [_, num, hasPlus, hasMinus, secondNumber] = match
-  const minus = hasMinus !== undefined && secondNumber === undefined ? -1 : 0 // check minus is not a V1-2
-  const plus =
-    (hasMinus !== undefined && secondNumber !== undefined) || hasPlus !== undefined ? 1 : 0 // grade V1+ the same as V1-2
-  score = (parseInt(num, 10) + 1) * 10 + minus + plus // V0 = 10, leave room for V-easy to be below 0
-
-  return score
 }
 
 const getScore = (grade: string): number | Tuple => {
-  const parse = grade.match(uiaaGradeRegex)
+  const parse = isUIAA(grade)
   if (parse == null) {
-    // not a valid UIAA scale
-    console.warn(`Unexpected grade format: ${grade} for grade scale UIAA scale`)
+    console.warn(`Unexpected grade format: ${grade} for grade scale UIAA`)
     return -1
   }
-  const [wholeMatch, basicGrade, plus, dash, secondGrade] = parse
+  const [wholeMatch, basicGrade, slash] = parse
+  const basicScore = findScoreRange((r: Route) => {
+    return r.uiaa === basicGrade
+  }, routes)
 
-  const basicScore = findScoreRange(
-    (r: Boulder) => r.v.toLowerCase() === basicGrade.toLowerCase(),
-    boulder
-  )
-  // ugly but working will fix later (says ever dev ever)
   if (wholeMatch !== basicGrade) {
     let otherGrade
-    // V5+, V2-3
-    if (plus !== undefined || secondGrade !== undefined) {
+    if (slash !== null) {
       otherGrade = (typeof basicScore === 'number' ? basicScore : basicScore[1]) + 1
-    } else if (dash !== undefined) {
-      // V5-, V2-
-      otherGrade = (typeof basicScore === 'number' ? basicScore : basicScore[0]) - 1
     }
     if (otherGrade !== undefined) {
       const nextGrade = findScoreRange(
-        (r: Boulder) => r.v.toLowerCase() === boulder[otherGrade].v.toLowerCase(),
-        boulder
+        (r: Route) => r.uiaa.toLowerCase() === routes[otherGrade].UIAA.toLowerCase(),
+        routes
       )
       return [getAvgScore(basicScore), getAvgScore(nextGrade)].sort((a, b) => a - b) as Tuple
     }
   }
   return basicScore
 }
+
+export default UIAAScale
